424cb2a08cb51ecf7d404ea2d1f9d257c19c1087
WassimLazaar <w.lazaar@student.avans.nl>
2024-09-11 13:53:59 +0200

    tijdelijke fixes

--- src/main.c
+++ src/main.c
@@ -45,6 +45,31 @@
 
 
 
+/*
+ * Get a device structure from a devicetree node with compatible
+ * "bosch,bme280". (If there are multiple, just pick one.)
+ */
+static const struct device *get_bme280_device(void)
+{
+	const struct device *const dev = DEVICE_DT_GET_ANY(bosch_bme280);
+
+	if (dev == NULL) {
+		/* No such node, or the node does not have status "okay". */
+		printk("\nError: no device found.\n");
+		return NULL;
+	}
+
+	if (!device_is_ready(dev)) {
+		printk("\nError: Device \"%s\" is not ready; "
+		       "check the driver initialization logs for errors.\n",
+		       dev->name);
+		return NULL;
+	}
+
+	printk("Found device \"%s\", getting sensor data\n", dev->name);
+	return dev;
+}
+
 /////////////////////////////////////////////////////////////////////////////////////
  struct printk_data_t {
 	void *fifo_reserved; /* 1st word reserved for use by fifo */
@@ -159,10 +184,23 @@ void blink3(void)
 
 void sensor(void)
 {
-
-while(1){
+const struct device *dev = get_bme280_device();
 
 	
+while(1){
+struct sensor_value temp, press, humidity;
+
+		sensor_sample_fetch(dev);
+		sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		sensor_channel_get(dev, SENSOR_CHAN_PRESS, &press);
+		sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &humidity);
+
+		printk("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
+		      temp.val1, temp.val2, press.val1, press.val2,
+		      humidity.val1, humidity.val2);
+
+		k_sleep(K_MSEC(1000));
+
 }
 
 }
