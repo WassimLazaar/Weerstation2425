cc6fdcb05fed8c86b431937e8b2a3b7b615eedc9
“Mikevaku” <mjb.vankuijk@student.avans.nl>
2024-10-21 16:13:16 +0200

    First version

--- src/main.c
+++ src/main.c
@@ -4,16 +4,44 @@
 //  * SPDX-License-Identifier: Apache-2.0
 //  */
 
-#include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/uart.h>
-
+// #include <zephyr/kernel.h>
+// #include <zephyr/device.h>
+// #include <zephyr/drivers/uart.h>
+#include "Weerstation.h"
 #include <string.h>
 #include <stdlib.h>
+
+#define STACKSIZE 1024
+#define PRIORITY 7
 
 #define UART_DEVICE_NODE DT_NODELABEL(usart1) //Define the uart from DT
 
 static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE); //Store in in variable uart_dev
+
+volatile int temperature;
+volatile int humidity;
+volatile int pressure;
+
+static const struct device *get_bme280_device(void)
+{
+	const struct device *const dev = DEVICE_DT_GET_ANY(bosch_bme280);
+
+	if (dev == NULL) {
+		/* No such node, or the node does not have status "okay". */
+		printk("\nError: no device found.\n");
+		return NULL;
+	}
+
+	if (!device_is_ready(dev)) {
+		printk("\nError: Device \"%s\" is not ready; "
+		       "check the driver initialization logs for errors.\n",
+		       dev->name);
+		return NULL;
+	}
+
+	printk("Found device \"%s\", getting sensor data\n", dev->name);
+	return dev;
+}
 
 //Function to send characters over UART
 void print_uart(char *buf)
@@ -25,16 +53,16 @@ void print_uart(char *buf)
 	}
 }
 
-int main(void)
+void esp(void)
 {
-	int temperature = 70;
-	int humidity = 100;
-	int pressure = 80;
+	// int temperature = 70;
+	// int humidity = 100;
+	// int pressure = 80;
 	const char* api_key = "ESPIsAFunDevice12345"; //API key extra protection
 
 	//Request string for posting data to webserver
 	const char* strRequest = "POST /Request_data_ESP/post-data.php HTTP/1.1\r\n"
-							 "Host: 172.20.10.2\r\n"
+							 "Host: 172.20.10.10\r\n"
 							 "Content-Type: application/x-www-form-urlencoded\r\n";
 
 	//Contionous flow of data to webserver
@@ -44,7 +72,7 @@ int main(void)
 		k_sleep(K_MSEC(2000));
 		print_uart("AT+CIFSR\r\n"); //validate IP address
 		k_sleep(K_MSEC(2000));
-		print_uart("AT+CIPSTART=\"TCP\",\"172.20.10.2\",80\r\n"); //Start server connection to localhost
+		print_uart("AT+CIPSTART=\"TCP\",\"172.20.10.10\",80\r\n"); //Start server connection to localhost
 		k_sleep(K_MSEC(2000));
 
 		//INIT data that needs to be posted to webserver (variables) 
@@ -87,6 +115,61 @@ int main(void)
 		k_sleep(K_MSEC(2000));
 	}
 	print_uart("AT+CIPCLOSE\r\n"); //After program end connection
-
-	return 0;
 }
+
+void sensor(void)
+{
+	const struct device *dev = get_bme280_device();
+	
+	while(1){
+		struct sensor_value temp, press, hum;
+
+		sensor_sample_fetch(dev);
+		sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		sensor_channel_get(dev, SENSOR_CHAN_PRESS, &press);
+		sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &hum);
+
+		temperature = temp.val1;
+		pressure = press.val1;
+		humidity = hum.val1;
+
+		// char buf[50];  // Buffer om de string op te slaan
+    	// //Maak de string en sla deze op in buf
+    	// int len = snprintf(buf, 50, 
+        //                "temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
+        //                temp.val1, temp.val2, 
+        //                press.val1, press.val2, 
+        //                humidity.val1, humidity.val2);
+		// //Controleer of snprintf succesvol was
+    	// if (len > 0 && len < 50) {
+        // print_uart(buf);  // Stuur de geformatteerde string via UART
+    	// } 	
+
+
+		// printk("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
+		//       temp.val1, temp.val2, press.val1, press.val2,
+		//       humidity.val1, humidity.val2);
+
+	// check_esp_connection(); // check of er verbinding is met de esp 
+	// send_to_esp(temp.val1, press.val1, humidity.val1);
+	// send_buffered_data();
+	// buffer_data(temp.val1, press.val1, humidity.val1);
+
+
+    //     if (temp.val1 >= 22 && temp.val1 < 25 ) {
+    //         temp_buiten = true;
+	// 		print_uart("temp tussen 22 en 25\n");
+
+    //     } 
+	// 	else {
+    //         temp_buiten = false;
+	// 		print_uart("temp buiten 22 en 25\n");
+    //     }
+		k_sleep(K_MSEC(1000));
+	}
+}
+
+K_THREAD_DEFINE(sensor_id, STACKSIZE, sensor, NULL, NULL, NULL,
+		PRIORITY, 0, 0);
+K_THREAD_DEFINE(esp_id, STACKSIZE, esp, NULL, NULL, NULL,
+		PRIORITY, 0, 0);
